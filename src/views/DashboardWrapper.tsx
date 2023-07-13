0e8e287a23d0392e4f32a850a2b5f234074f228a
Giwa Muhammad <mgiwa78@gmail.com>
2023-07-13 11:20:16 +0100

    Added adverts to dashboard Minor fix

--- src/views/DashboardWrapper.tsx
+++ src/views/DashboardWrapper.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {FC} from 'react'
+
+import '../assets/styles/ads.css'
 // import {toAbsoluteUrl} from '../../helpers'
 import {PageTitle} from '../layouts/core'
 
@@ -8,42 +10,34 @@ const DashboardPage: FC = () => (
     {/* begin::Row */}
     <div className='row g-5 g-xl-10 mb-5 mb-xl-10'>
       <div className='row'>
-        <div className='col-6 my-10'>
+        <div
+          className='col-12 col-lg-6 my-10 ads-1'
+          style={{
+            backgroundImage: `url("/media/ads/1.jpg")`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        ></div>
+        <div className='col-12 col-lg-6 my-10'>
           <div
-            className='card'
-            style={{
-              backgroundImage: `url("/media/ads/1.jpg")`,
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              height: 500,
-              width: '100%',
-            }}
-          ></div>
-        </div>
-        <div className='col-6 my-10'>
-          <div
-            className='card'
+            className='card ads-2'
             style={{
               backgroundImage: `url("/media/ads/2.jpg")`,
               backgroundPosition: 'center',
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
-              height: 500,
-              width: '100%',
             }}
           ></div>
         </div>
-        <div className='col-6 my-10'>
+        <div className='col-12 col-xl-6 my-10'>
           <div
-            className='card'
+            className='card ads-3'
             style={{
               backgroundImage: `url("/media/ads/3.jpg")`,
               backgroundPosition: 'center',
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
-              height: 700,
-              width: '100%',
             }}
           ></div>
         </div>
